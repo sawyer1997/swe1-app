34fe23b36b63a1e1bf4e40472f996044cac2eed7
sawyer1997 <rishabhverma220898@gmail.com>
2023-10-14 15:53:20 -0400

    Added coverage/coveralls and updated travis

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-1$xs4!a-_o6@o&onws0%lj&u=@osb7l1-!j8j5*%85iy(vh0gh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django-env.eba-xhvezc6h.us-east-1.elasticbeanstalk.com', 'localhost']
+ALLOWED_HOSTS = ['django-env.eba-xhvezc6h.us-east-1.elasticbeanstalk.com', 'localhost', 'django-test-env.eba-xhvezc6h.us-east-1.elasticbeanstalk.com', 'django-env.eba-68zt3geb.us-west-2.elasticbeanstalk.com']
 
 
 # Application definition
