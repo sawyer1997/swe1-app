8df269ba83fd507987fdf5b5aa3edd63c144d4c9
sawyer1997 <rishabhverma220898@gmail.com>
2023-10-14 21:01:21 -0400

    Edited files path

--- test_sample.py
+++ test_sample.py
@@ -1,12 +1,15 @@
 # context of test_sample.py
 def func(x):
-    return x+1
+    return x + 1
+
 
 def fun(x):
-    return x*x
+    return x * x
+
 
 def test_answer():
     assert func(3) == 4
+
 
 def test_square():
     assert fun(4) == 16
